404c236830852c01a2adc0f578e97ca3819b7223
simmohamed <44633169+simmohamed@users.noreply.github.com>
2018-10-31 10:32:01 +0200

    Add files via upload

---
+++ sketch_oct30b11.ino
@@ -0,0 +1,131 @@
+
+int potentiometer = A0;
+int potval;
+
+const int pinA = 6;
+int pinB = 5;
+int pinC = 4;
+int pinD = 3;
+int pinE = 2;
+int pinF = 7;
+int pinG = 8;
+
+
+
+void setup() {                
+  pinMode(pinA, OUTPUT);     
+  pinMode(pinB, OUTPUT);     
+  pinMode(pinC, OUTPUT);     
+  pinMode(pinD, OUTPUT);     
+  pinMode(pinE, OUTPUT);     
+  pinMode(pinF, OUTPUT);     
+  pinMode(pinG, OUTPUT);     
+}
+
+void loop() {
+  potval=analogRead(potentiometer);
+  if (potval>=0 && potval<=101){
+    //0
+    digitalWrite(pinA, LOW);   
+    digitalWrite(pinB, LOW);   
+    digitalWrite(pinC, LOW);   
+    digitalWrite(pinD, LOW);   
+    digitalWrite(pinE, LOW);   
+    digitalWrite(pinF, LOW);   
+    digitalWrite(pinG, HIGH);     
+    }
+  else if (potval>=102 && potval<=203){
+     //1
+    digitalWrite(pinA, HIGH);   
+    digitalWrite(pinB, LOW);   
+    digitalWrite(pinC, LOW);   
+    digitalWrite(pinD, HIGH);   
+    digitalWrite(pinE, HIGH);   
+    digitalWrite(pinF, HIGH);   
+    digitalWrite(pinG, HIGH);
+    }
+  else if (potval>=204 && potval<=305){
+     //2
+    digitalWrite(pinA, LOW);   
+    digitalWrite(pinB, LOW);   
+    digitalWrite(pinC, HIGH);   
+    digitalWrite(pinD, LOW);   
+    digitalWrite(pinE, LOW);   
+    digitalWrite(pinF, HIGH);   
+    digitalWrite(pinG, LOW);  
+    }
+  else if (potval>=306 && potval<=407){
+    //3
+    digitalWrite(pinA, LOW);   
+    digitalWrite(pinB, LOW);   
+    digitalWrite(pinC, LOW);   
+    digitalWrite(pinD, LOW);   
+    digitalWrite(pinE, HIGH);   
+    digitalWrite(pinF, HIGH);   
+    digitalWrite(pinG, LOW);  
+    }
+  else if (potval>=408 && potval<=509){
+    //4
+    digitalWrite(pinA, HIGH);   
+    digitalWrite(pinB, LOW);   
+    digitalWrite(pinC, LOW);   
+    digitalWrite(pinD, HIGH);   
+    digitalWrite(pinE, HIGH);   
+    digitalWrite(pinF, LOW);   
+    digitalWrite(pinG, LOW);     
+    }
+  else if (potval>=510 && potval<=611){
+    //5
+    digitalWrite(pinA, LOW);   
+    digitalWrite(pinB, HIGH);   
+    digitalWrite(pinC, LOW);   
+    digitalWrite(pinD, LOW);   
+    digitalWrite(pinE, HIGH);   
+    digitalWrite(pinF, LOW);   
+    digitalWrite(pinG, LOW);     
+    }
+  else if (potval>=612 && potval<=713){
+    //6
+    digitalWrite(pinA, LOW);   
+    digitalWrite(pinB, HIGH);   
+    digitalWrite(pinC, LOW);   
+    digitalWrite(pinD, LOW);   
+    digitalWrite(pinE, LOW);   
+    digitalWrite(pinF, LOW);   
+    digitalWrite(pinG, LOW);     
+    }
+  else if (potval>=714 && potval<=815){
+    //7
+    digitalWrite(pinA, LOW);   
+    digitalWrite(pinB, LOW);   
+    digitalWrite(pinC, LOW);   
+    digitalWrite(pinD, HIGH);   
+    digitalWrite(pinE, HIGH);   
+    digitalWrite(pinF, HIGH);   
+    digitalWrite(pinG, HIGH);      
+    }
+  else if (potval>=816 && potval<=917){
+    //8
+    digitalWrite(pinA, LOW);   
+    digitalWrite(pinB, LOW);   
+    digitalWrite(pinC, LOW);   
+    digitalWrite(pinD, LOW);   
+    digitalWrite(pinE, LOW);   
+    digitalWrite(pinF, LOW);   
+    digitalWrite(pinG, LOW);         
+    }
+  else if (potval>=917 && potval<=1023){
+    //9
+    digitalWrite(pinA, LOW);   
+    digitalWrite(pinB, LOW);   
+    digitalWrite(pinC, LOW);   
+    digitalWrite(pinD, LOW);   
+    digitalWrite(pinE, HIGH);   
+    digitalWrite(pinF, LOW);   
+    digitalWrite(pinG, LOW);         
+    }
+    
+    
+    
+
+}
